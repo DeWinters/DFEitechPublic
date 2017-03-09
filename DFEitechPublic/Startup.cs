b4a8bc42e5bb5835bd56b996d0b28b34d7a6bbd5
DeWinters <derekwwinters@gmail.com>
2017-03-09 09:11:59 +0000

    Add project files.

---
+++ DFEitechPublic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DFEitechPublic.Startup))]
+namespace DFEitechPublic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
